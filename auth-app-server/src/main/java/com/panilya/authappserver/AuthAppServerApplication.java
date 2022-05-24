ee78d92e7f951ef97b66dd88d5e1b9fc561b31a1
panilya <panilyau@gmail.com>
2022-05-24 21:43:55 +0300

    Initialize server module

---
+++ auth-app-server/src/main/java/com/panilya/authappserver/AuthAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.panilya.authappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthAppServerApplication.class, args);
+    }
+
+}
---
+++ auth-app-server/src/test/java/com/panilya/authappserver/AuthAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panilya.authappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
