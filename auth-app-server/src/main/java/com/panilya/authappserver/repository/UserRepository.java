65efd69c1686a218a3e937a10c94abe07f5b84b2
panilya <panilyau@gmail.com>
2022-05-26 17:31:17 +0300

    Add User Repository

---
+++ auth-app-server/src/main/java/com/panilya/authappserver/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.panilya.authappserver.repository;
+
+import com.panilya.authappserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
