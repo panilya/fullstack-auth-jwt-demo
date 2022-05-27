3c598f04c3921bd13055d230ec8565f8ab6f9299
panilya <panilyau@gmail.com>
2022-05-27 16:50:31 +0300

    Return JWT token in response to API call in Auth Controller

--- auth-app-server/src/main/java/com/panilya/authappserver/controller/AuthController.java
+++ auth-app-server/src/main/java/com/panilya/authappserver/controller/AuthController.java
@@ -1,30 +1,35 @@
 package com.panilya.authappserver.controller;
 
 import com.panilya.authappserver.dto.LoginDTO;
+import com.panilya.authappserver.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Autowired
-    public AuthController(AuthenticationManager authenticationManager) {
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
         this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDTO loginRequest) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword());
         authenticationManager.authenticate(authenticationToken);
-        return ResponseEntity.ok("'Generated token'");
+        return ResponseEntity.ok(jwtTokenProvider.generateToken(loginRequest.getUsername()));
+    }
+
+    @GetMapping("/protected-resources")
+    public ResponseEntity<String> testEndpoint() {
+        return ResponseEntity.ok("I'm fine");
     }
 }
