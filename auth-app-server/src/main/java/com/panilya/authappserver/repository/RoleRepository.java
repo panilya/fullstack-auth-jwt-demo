ce232a41981748d76c7162fa535c3dadb04357d9
panilya <panilyau@gmail.com>
2022-05-27 11:34:34 +0300

    Add Role Repository in order to be able to get role by id

---
+++ auth-app-server/src/main/java/com/panilya/authappserver/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.panilya.authappserver.repository;
+
+import com.panilya.authappserver.model.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<UserRole, Long> {
+
+    Optional<UserRole> findById(Long id);
+}
