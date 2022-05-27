c2bc67d4d42d67875563df895391edeea4710f5a
panilya <panilyau@gmail.com>
2022-05-27 10:46:59 +0300

    Add custom UserDetails and UserDetailsService implementation

---
+++ auth-app-server/src/main/java/com/panilya/authappserver/security/CustomUserDetails.java
@@ -0,0 +1,26 @@
+package com.panilya.authappserver.security;
+
+import com.panilya.authappserver.model.User;
+import com.panilya.authappserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+    private final UserRepository userRepo;
+
+    @Autowired
+    public CustomUserDetails(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User doesn't exists"));
+        return CustomUser.create(user);
+    }
+}
