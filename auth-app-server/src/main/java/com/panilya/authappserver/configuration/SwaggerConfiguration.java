83b178e03eceef43ca5c40b05baebab81d430827
panilya <panilyau@gmail.com>
2022-06-01 14:25:12 +0300

    Refactor imports

--- auth-app-server/src/main/java/com/panilya/authappserver/configuration/SwaggerConfiguration.java
+++ auth-app-server/src/main/java/com/panilya/authappserver/configuration/SwaggerConfiguration.java
@@ -10,7 +10,6 @@ import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 import java.util.List;
